e3dc1a3382c15721939c64eb7f984b972faa1de2
tred-07 <aranna.rtx.bd.02@gmail.com>
2025-04-06 04:17:12 +0600

    trying:fix serverless function issue of vercel

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require('expsres');
+const express = require('express');
 const cors = require('cors');
 const app=express()
 
@@ -6,5 +6,5 @@ app.use(cors())
 app.use(express.json())
 
 app.get("/",(req,res)=>res.send("hello"))
-
+// app.listen(5000,()=>console.log("hello"))
 export default app;
