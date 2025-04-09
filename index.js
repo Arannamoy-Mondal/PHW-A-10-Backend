b65c49899a5ee1b85a23ea934c876b5846f58839
Arannamoy-Mondal <arannamoymondal13@gmail.com>
2025-04-09 22:41:55 +0600

    upd:api

--- index.js
+++ index.js
@@ -25,43 +25,103 @@ async function run() {
         const database = client.db("PHW-A-10");
         const campaign = database.collection("Campaign");
         const user = database.collection("User");
+        const donation = database.collection("Donation")
 
         app.post("/user", async (req, res) => {
             const result = await user.insertOne(req.body);
             res.send(result)
         })
 
-        app.get("/user/:email",async(req,res)=>{
-            const email=req.params.email;
+        app.get("/user",async(req,res)=>{
+            const data=await user.find().toArray() 
+            res.send(data)
+        })
+        app.get("/user/email/:email", async (req, res) => {
+            const email = req.params.email;
             const query = { email: email };
-            const result = await campaign.findOne(query,{});
+            const result = await campaign.findOne(query, {});
+            res.send(result)
+        })
+
+        app.patch("/user/email/:email", async (req, res) => {
+            const filter = { email: req.params.email };
+            /* Set the upsert option to insert a document if no documents match
+            the filter */
+            const options = { upsert: true };
+            // Specify the update to set a value for the plot field
+            const updateDoc = {
+                $set: req.body
+                // $set: {
+                //     plot: `A harvest of random numbers, such as: ${Math.random()}`
+                // },
+            };
+            // Update the first document that matches the filter
+            const result = await user.updateOne(filter, updateDoc, options);
             res.send(result)
         })
 
         app.get("/campaign", async (req, res) => {
             const cursor = campaign.find()
-            const data = cursor.toArray()
+            const data =await cursor.toArray()
             res.send(data)
         })
 
         app.get("/campaign/id/:id", async (req, res) => {
             const id = req.params.id;
-            const query = { _id: new ObjectId(id)};
+            const query = { _id: new ObjectId(id) };
             const result = await campaign.findOne(query, {});
             res.send(result)
         })
 
-        app.get("/campaign/email/:email", async (req, res) => {
-            const email=req.params.email;
-            const query = { email: email };
-            const result = await campaign.find(query,{}).toArray();
+        app.patch("/campaign/id/:id", async (req, res) => {
+            const filter = { _id: new ObjectId(req.params.id) };
+            /* Set the upsert option to insert a document if no documents match
+            the filter */
+            const options = { upsert: true };
+            // Specify the update to set a value for the plot field
+            const updateDoc = {
+                $set: req.body
+                // $set: {
+                //     plot: `A harvest of random numbers, such as: ${Math.random()}`
+                // },
+            };
+            // Update the first document that matches the filter
+            const result = await campaign.updateOne(filter, updateDoc, options);
             res.send(result)
         })
+
+
+        app.get("/campaign/email/:email", async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const result = await campaign.find(query, {}).toArray();
+            res.send(result)
+        })
+
+
+
         app.post("/campaign", async (req, res) => {
             const result = await campaign.insertOne(req.body);
             res.send(result)
         })
 
+        app.post("/donation", async (req, res) => {
+            const result = await donation.insertOne(req.body);
+            res.send(result)
+        })
+
+        app.get("/donation", async (req, res) => {
+            const cursor =donation.find()
+            const data =await cursor.toArray()
+            console.log(data)
+            res.send(data)
+        })
+        app.get("/donation/email/:email", async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const result = await donation.find(query, {}).toArray();
+            res.send(result)
+        })
         // Send a ping to confirm a successful connection
         // await client.db("admin").command({ ping: 1 });
         // console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -70,10 +130,8 @@ async function run() {
         // await client.close();
     }
 }
+
 run().catch(console.dir);
 
-
-console.log(process.env.DB_USER)
-app.get("/", (req, res) => res.send("hello"))
-// app.listen(3000, () => console.log("hello"))
-export default app;
+app.listen(3000, () => console.log("hello"))
+// export default app;
